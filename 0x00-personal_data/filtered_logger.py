994c933a93203e1c2f1435d608542b6043d21a7b
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-08-19 15:59:30 +0000

    feat task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,3 +14,24 @@ def filter_datum(fields: List[str], redaction: str,
         temp = re.sub(field + "=.*?" + separator,
                       field + "=" + redaction + separator, temp)
     return temp
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ inits class instance """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filters values in incoming log records """
+        return filter_datum(
+            self.fields, self.REDACTION, super(
+                RedactingFormatter, self).format(record),
+            self.SEPARATOR)
