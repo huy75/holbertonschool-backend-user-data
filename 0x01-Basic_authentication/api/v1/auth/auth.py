7168b043c237b7fe34440fe0783ec33cf2341b09
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-08-23 14:42:54 +0000

    feat tasks 4 - 6

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,10 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ Checks if Authorization request header is present
         & contains values """
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
         
     def current_user(self, request=None) -> TypeVar('User'):
         """ placeholder """
