6b6af074ac3d80bbb0873f2abc06c92f82a3c71d
huy75 <huy.nguyen@holbertonschool.com>
2021-08-26 11:46:05 +0000

    feat task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-"""
-API session authentication module
-"""
+"""API session authentication module"""
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Authentication """
-    pass 
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for user_id """
+
+        if user_id is None or isinstance(user_id, str) is False:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
