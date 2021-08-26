2ea69c0fed185255e31f601743381c9e8b05028d
huy75 <huy.nguyen@holbertonschool.com>
2021-08-26 09:10:41 +0000

    feat task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+API session authentication module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session Authentication """
+    pass 
