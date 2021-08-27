e94f7942e9b506e8940684e9320bff3424a6e557
huy75 <huy.nguyen@holbertonschool.com>
2021-08-27 12:58:27 +0000

    fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -39,13 +39,10 @@ class SessionAuth(Auth):
     def destroy_session(self, request=None):
         """ Deletes user session to logout """
 
-        if request is None or not self.session_cookie(request):
+        if request is None:
             return False
-        if self.user_id_for_session_id(request) is None or\
-                self.user_id_for_session_id(self.session_cookie(request)):
+        cookie = self.session_cookie(request)
+        if cookie is None or self.user_id_for_session_id(cookie) is None:
             return False
-
-        session_id = self.session_cookie(request)
-
-        del self.user_id_by_session_id[session_id]
+        del self.user_id_by_session_id[cookie]
         return True
