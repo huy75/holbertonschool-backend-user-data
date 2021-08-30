6a4f9272d2ca17ddcfd0a28240b5171d6e6cf10b
huy75 <huy.nguyen@holbertonschool.com>
2021-08-30 15:30:56 +0000

    fix session_auth

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -38,9 +38,9 @@ def session_login() -> str:
     user_email = request.form.get('email')
     user_pswd = request.form.get('password')
 
-    if user_email is None:
+    if not user_email:
         return jsonify({"error": "email missing"}), 400
-    if user_pswd is None:
+    if not user_pswd:
         return jsonify({"error": "password missing"}), 400
 
     try:
@@ -51,12 +51,12 @@ def session_login() -> str:
         return jsonify({"error": "no user found for this email"}), 404
 
     for user in search_users:
-        if user.is_valid_password(user_pswd):
-            from api.v1.app import auth
-            session_cookie = getenv("SESSION_NAME")
-            session_id = auth.create_session(user.id)
-            response = jsonify(user.to_json())
-            response.set_cookie(session_cookie, session_id)
-            return response
-        else:
+        if not user.is_valid_password(user_pswd):
             return jsonify({"error": "wrong password"}), 401
+        from api.v1.app import auth
+        session_cookie = getenv("SESSION_NAME")
+        session_id = auth.create_session(user.id)
+        response = jsonify(user.to_json())
+        response.set_cookie(session_cookie, session_id)
+        return response
+    return jsonify({"error": "no user found for this email"}), 404
