b2a8b1272d1f70e4dc43db8bf2eee2f5cd30269b
huy75 <huy.nguyen@holbertonschool.com>
2021-09-01 12:09:43 +0000

    feat task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,3 +9,20 @@ def _hash_password(password: str) -> str:
     Returns salted, hashed pswd as bytestring
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ Instance """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Registers and returns a new user if email isn't listed"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except Exception:
+            return self._db.add_user(email, _hash_password(password))
