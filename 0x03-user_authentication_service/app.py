291bda6a803705bc5cfacdf26629d807fa02492e
huy75 <huy.nguyen@holbertonschool.com>
2021-09-01 12:33:09 +0000

    feat task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Route module for the API """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -12,6 +12,39 @@ def status() -> str:
       - JSON payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def new_user() -> str:
+    """ POST /users
+    Registers new user with email and pswd in request form-data,
+    or finds if user already registered based on email
+    Return:
+      - JSON payload
+    """
+
+    # Get data from form request, change to request.get_json() for body
+    form_data = request.form
+
+    if "email" not in form_data:
+        return jsonify({"message": "email required"}), 400
+    elif "password" not in form_data:
+        return jsonify({"message": "password required"}), 400
+    else:
+
+        email = form_data["email"]
+        pswd = form_data["password"]
+
+        try:
+            new_user = AUTH.register_user(email, pswd)
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            }), 201
+        except ValueError:
+            return jsonify({
+                "message": "user already registered with this email"
+                }), 400
 
 
 if __name__ == "__main__":
