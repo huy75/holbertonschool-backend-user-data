91de20f226ac24df18fb0955e96da81ef6ddcf0c
huy75 <huy.nguyen@holbertonschool.com>
2021-09-01 17:04:48 +0000

    fix return

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,10 +89,12 @@ def profile() -> str:
         - 403 if session ID is invalid
     """
     user_cookie = request.cookies.get("session_id", None)
-    user = AUTH.get_user_from_session_id(user_cookie)
-    if user_cookie is None or user is None:
+    if user_cookie is None:
         abort(403)
-    return jsonify({"email": user}), 200
+    user = AUTH.get_user_from_session_id(user_cookie)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
 
 
 if __name__ == "__main__":
